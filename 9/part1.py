d5c43a50d192b5b552415deb4cac7e1f57e633d6
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-12-09 11:24:16 +0000

    day 9

---
+++ 9/part1.py
@@ -0,0 +1,15 @@
+nexts = 0
+
+with open("input") as f:
+    for line in f:
+        numbers = [int(i) for i in line.strip().split()]
+
+        diffs = [numbers]
+        while max(diffs[-1]) != min(diffs[-1]) or diffs[-1][0] != 0:
+            diffs.append([j-i for i, j in zip(diffs[-1][:-1], diffs[-1][1:])])
+
+        diffs[-1].append(0)
+        for i in range(len(diffs) - 2, -1, -1):
+            diffs[i].append(diffs[i][-1] + diffs[i+1][-1])
+        nexts += diffs[0][-1]
+print(nexts)
